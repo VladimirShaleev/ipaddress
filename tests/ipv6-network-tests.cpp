0a113e9439c1ca5747578824e43b599f575bc806
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-01-30 03:15:45 +0300

    Add unit test for ipv6_network

--- tests/ipv6-network-tests.cpp
+++ tests/ipv6-network-tests.cpp
@@ -84,3 +84,37 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple("::/0", "::", "::", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", 0),
         std::make_tuple("::/128", "::", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", "::", 128)
     ));
+
+using NetworkParserIpv6NotStrictParams = TestWithParam<std::tuple<const char*, const char*, const char*, const char*, size_t>>;
+TEST_P(NetworkParserIpv6NotStrictParams, parse_not_strict) {
+    auto excepted_address = ipv6_address::parse(get<1>(GetParam()));
+    auto excepted_netmask = ipv6_address::parse(get<2>(GetParam()));
+    auto excepted_hostmask = ipv6_address::parse(get<3>(GetParam()));
+    auto excepted_prefixlen = get<4>(GetParam());
+
+    auto net = ipv6_network::parse(get<0>(GetParam()), false);
+   
+    EXPECT_EQ(net.address(), excepted_address);
+    EXPECT_EQ(net.netmask(), excepted_netmask);
+    EXPECT_EQ(net.hostmask(), excepted_hostmask);
+    EXPECT_EQ(net.prefixlen(), excepted_prefixlen);
+
+    std::string s1;
+    ipv6_network net_from_stream;
+    std::string s2;
+    std::istringstream ss(std::string("test: ") + get<0>(GetParam()) + " parser");
+    ss >> s1 >> not_strict >> net_from_stream >> s2;
+
+    ASSERT_EQ(s1, std::string("test:"));
+    EXPECT_EQ(net_from_stream.address(), excepted_address);
+    EXPECT_EQ(net_from_stream.netmask(), excepted_netmask);
+    EXPECT_EQ(net_from_stream.hostmask(), excepted_hostmask);
+    EXPECT_EQ(net_from_stream.prefixlen(), excepted_prefixlen);
+    ASSERT_EQ(s2, std::string("parser"));
+}
+INSTANTIATE_TEST_SUITE_P(
+    ipv6_network, NetworkParserIpv6NotStrictParams,
+    testing::Values(
+        std::make_tuple("2001:db8::/16", "2001::", "ffff::", "0:ffff:ffff:ffff:ffff:ffff:ffff:ffff", 16),
+        std::make_tuple("2001:db8::/24", "2001:d00::", "ffff:ff00::", "0:ff:ffff:ffff:ffff:ffff:ffff:ffff", 24)
+    ));
