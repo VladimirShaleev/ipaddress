71e7dd9df5ff24d3e4d618fef17fce1cd4954454
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-02-24 09:06:03 +0300

    Update byte_array_type

--- include/ipaddress/byte-array.hpp
+++ include/ipaddress/byte-array.hpp
@@ -522,12 +522,14 @@ IPADDRESS_NODISCARD IPADDRESS_CONSTEXPR IPADDRESS_FORCE_INLINE bool operator!=(c
  *       the ability to use constexpr if available.
  */
 template <std::size_t N>
-using byte_array_type = 
-#if IPADDRESS_CPP_VERSION >= 20
-    std::array<uint8_t, N>;
-#else
-    byte_array<N>;
-#endif
+using byte_array_type = byte_array<N>;
+// template <std::size_t N>
+// using byte_array_type = 
+// #if IPADDRESS_CPP_VERSION >= 20 && (!defined(__clang_major__) || __clang_major__ > 10)
+//     std::array<uint8_t, N>;
+// #else
+//     byte_array<N>;
+// #endif
 
 } // namespace IPADDRESS_NAMESPACE
 
