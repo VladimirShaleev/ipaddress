fec99233416a175c4eacc9bd0083c2ecc520ad40
Vladimir Shaleev <vladimirshaleev@gmail.com>
2024-02-25 13:57:38 +0300

    Add doc for ipv6-network

--- include/ipaddress/ipv6-network.hpp
+++ include/ipaddress/ipv6-network.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file      ipv6-network.hpp
+ * @brief     Provides a set of functions and classes for handling IPv6 networks
+ * @author    Vladimir Shaleev
+ * @copyright MIT License
+ * 
+ * This header file defines the ipv6_network_base class and ipv6_network type, which are
+ * part of a library for working with IPv6 network addresses. The ipv6_network_base class
+ * is derived from the base_v6 class and includes methods for manipulating network addresses.
+ * The ipv6_network is a typedef for the ip_network_base class specialized for IPv6, 
+ * providing a convenient alias for users of the library.
+ */
+
 #ifndef IPADDRESS_IPV6_NETWORK_HPP
 #define IPADDRESS_IPV6_NETWORK_HPP
 
@@ -6,10 +19,25 @@
 
 namespace IPADDRESS_NAMESPACE {
 
+/**
+ * Base class for IPv6 network address manipulation.
+ * 
+ * The ipv6_network_base class provides foundational functionality for IPv6 network
+ * address manipulation by extending the base_v6 class.
+ */
 class ipv6_network_base : public base_v6<ipv6_network_base> {
 public:
-    using ip_address_type = ipv6_address;
+    using ip_address_type = ipv6_address; /**< Alias for the IPv6 address type used within the class. */
 
+    /**
+     * Checks if the IPv6 network is site-local.
+     * 
+     * This method determines if both the network address and the broadcast address
+     * of the IPv6 network are site-local, which are addresses used within a particular
+     * organization's intranet and are not routable on the global internet.
+     * 
+     * @return A boolean value indicating whether the network is site-local.
+     */
     IPADDRESS_NODISCARD IPADDRESS_CONSTEXPR IPADDRESS_FORCE_INLINE bool is_site_local() const IPADDRESS_NOEXCEPT {
         const auto& network = *static_cast<const ip_network_base<ipv6_network_base>*>(this);
         return network.network_address().is_site_local() && network.broadcast_address().is_site_local();
@@ -27,10 +55,27 @@ protected:
     }
 }; // ipv6_network_base
 
+/**
+ * Alias for the specialized ip_network_base class for IPv6.
+ * 
+ * The ipv6_network is a convenient alias for the ip_network_base class
+ * specialized with ipv6_network_base. It allows users to work with IPv6
+ * network addresses using a type that is specifically designed for IPv6,
+ * simplifying the interface and usage in code that deals with IPv6 networks.
+ */
 using ipv6_network = ip_network_base<ipv6_network_base>;
 
 #ifdef IPADDRESS_NONTYPE_TEMPLATE_PARAMETER
 
+    /**
+     * User-defined literal operator for creating an ipv6_network object from a string literal.
+     * 
+     * This operator allows the creation of ipv6_network objects using a string literal with the
+     * `_ipv6_net` suffix.
+     * 
+     * @tparam    FixedString A string literal representing the IPv6 network.
+     * @return    An ipv6_network object representing the network specified by the string literal.
+     */
     template <fixed_string FixedString>
     IPADDRESS_NODISCARD IPADDRESS_CONSTEVAL IPADDRESS_FORCE_INLINE ipv6_network operator""_ipv6_net() IPADDRESS_NOEXCEPT {
         return ipv6_network::parse<FixedString>();
@@ -38,9 +83,57 @@ using ipv6_network = ip_network_base<ipv6_network_base>;
 
 #else // IPADDRESS_NONTYPE_TEMPLATE_PARAMETER
 
+    /**
+     * User-defined literal operator for creating an ipv6_network object from a string literal.
+     * 
+     * This operator allows the creation of ipv6_network objects using a string literal with the
+     * `_ipv6_net` suffix.
+     * 
+     * @param     address The string literal representing the IPv6 network.
+     * @param     size The size of the string literal.
+     * @return    An ipv6_network object representing the network specified by the string literal.
+     */
     IPADDRESS_NODISCARD IPADDRESS_CONSTEXPR IPADDRESS_FORCE_INLINE ipv6_network operator""_ipv6_net(const char* address, std::size_t size) IPADDRESS_NOEXCEPT {
         assert(size <= ipv6_network::base_max_string_len + 4 && "literal string is too long");
         char str[ipv6_network::base_max_string_len + 5] = {};
+        for (size_t i = 0; i < size; ++i) {
+            str[i] = address[i];
+        }
+        return ipv6_network::parse(str);
+    }
+
+    /**
+     * User-defined literal operator for creating an ipv6_network object from a wide string literal.
+     * 
+     * This operator allows the creation of ipv6_network objects using a string literal with the
+     * `_ipv6_net` suffix.
+     * 
+     * @param     address The string literal representing the IPv6 network.
+     * @param     size The size of the string literal.
+     * @return    An ipv6_network object representing the network specified by the string literal.
+     */
+    IPADDRESS_NODISCARD IPADDRESS_CONSTEXPR IPADDRESS_FORCE_INLINE ipv6_network operator""_ipv6_net(const wchar_t* address, std::size_t size) IPADDRESS_NOEXCEPT {
+        assert(size <= ipv6_network::base_max_string_len + 4 && "literal string is too long");
+        wchar_t str[ipv6_network::base_max_string_len + 5] = {};
+        for (size_t i = 0; i < size; ++i) {
+            str[i] = address[i];
+        }
+        return ipv6_network::parse(str);
+    }
+
+    /**
+     * User-defined literal operator for creating an ipv6_network object from UTF-32 string literal.
+     * 
+     * This operator allows the creation of ipv6_network objects using a string literal with the
+     * `_ipv6_net` suffix.
+     * 
+     * @param     address The string literal representing the IPv6 network.
+     * @param     size The size of the string literal.
+     * @return    An ipv6_network object representing the network specified by the string literal.
+     */
+    IPADDRESS_NODISCARD IPADDRESS_CONSTEXPR IPADDRESS_FORCE_INLINE ipv6_network operator""_ipv6_net(const char32_t* address, std::size_t size) IPADDRESS_NOEXCEPT {
+        assert(size <= ipv6_network::base_max_string_len + 4 && "literal string is too long");
+        char32_t str[ipv6_network::base_max_string_len + 5] = {};
         for (size_t i = 0; i < size; ++i) {
             str[i] = address[i];
         }
